f78c2fdabe8666c757c87d0a557858a9aa6193d9
labstraction <45163263+labstraction@users.noreply.github.com>
2023-05-16 21:22:22 +0200

    test

--- index.js
+++ index.js
@@ -1,14 +1,70 @@
-const express = require("express");
-const cors = require('cors');
+import fetch from 'node-fetch';
+import express from 'express';
+import cors from 'cors';
+
 const app = express();
+
+const openaiApiKey = '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';
+const temperature = 0.5;
+const url = 'https://api.openai.com/v1/chat/completions';
+const options = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${openaiApiKey}`,
+  },
+}
+
+function tryParseJSON(jsonString){
+  try {
+    return JSON.parse(jsonString);
+  }
+  catch (e) {
+    return {choices: [{delta: {content: ''}}]}
+  }
+}
+
+
+
 app.use(cors({
-    origin: '*'
+  origin: '*'
 }));
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+
+app.get("/", async (req, res) => {
+
+  const body = JSON.stringify({
+    messages: [
+      { "role": "user", "content": "Scrivi una storia di 1000 parole" }
+    ],
+    temperature: temperature,
+    stream: true,
+    model: "gpt-3.5-turbo",
+  });
+
+  const response = await fetch(url, { ...options, body })
+ 
+  try {
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8', 'Transfer-Encoding': 'chunked' });
+    for await (const chunk of response.body) {
+      const lines = chunk.toString('utf-8').split('\n').filter(line => line.trim() !== '');
+      const jsonLines = lines.map(line => line.replace(/^data: /, '')).filter(line => line.trim() !== '');
+      const text = jsonLines.reduce((acc, line) => {
+        const words = tryParseJSON(line).choices[0].delta.content;
+        return line !== '[DONE]' ? acc + (words !== undefined ? words : '') : acc;
+      }
+      , '');
+      res.write(text, 'utf-8');
+      
+      
+
+    }
+    res.end();
+  } catch (err) {
+    res.send(err.stack);
+  }
 });
+
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
-// Export the Express API
-module.exports = app;
+
